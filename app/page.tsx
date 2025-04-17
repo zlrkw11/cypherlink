596b2ffbb3e918d1f3e7dc21a490b8250c9c9803
ZL <ray040406@gmail.com>
2025-04-17 23:30:15 +1200

    feat: encrypt page skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <div className="grid items-center justify-items-center min-h-screen">
-      Hello World
+    <div className="flex flex-col h-screen items-center justify-center gap-2">
+      <h1 className="m-2">Encrypt a message</h1>
+      <input
+        type="text"
+        placeholder="enter your message..."
+        className="border border-gray-800 rounded-md p-2"
+      />
+      <button className="bg-blue-500 text-gray-100 rounded-md p-2 hover:bg-blue-300 cursor-pointer">
+        generate
+      </button>
     </div>
   );
 }
